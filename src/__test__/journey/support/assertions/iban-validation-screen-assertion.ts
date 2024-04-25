fd4cea9c13f2a51c643692456ccd86c26e25a837
paulbraganza12 <paulbraganza12@gmail.com>
2024-04-25 20:58:55 +0100

    fix: updates valid iban assertions

--- src/__test__/journey/support/assertions/iban-validation-screen-assertion.ts
+++ src/__test__/journey/support/assertions/iban-validation-screen-assertion.ts
@@ -1,10 +1,7 @@
 export class IbanValidationScreenAssertions {
   ibanValidDisplayed() {
-    cy.contains("Iban Valid").should("be.visible");
-    cy.contains("Trusted bank").should("be.visible");
-    cy.contains("Accepts instant payments").should("be.visible");
+    cy.contains("Valid IBAN").should("be.visible");
     cy.contains("Positive operation history").should("be.visible");
-    cy.contains("No security claims").should("be.visible");
     cy.contains("Complies with Payment Services Directive (PSD2)").should("be.visible");
   }
 }
